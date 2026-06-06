6bc8438a3d79c8cfeb0b8d4eddf8463679a0603d
ammermali <115208836+ammermali@users.noreply.github.com>
2026-06-07 01:51:49 +0200

    Updated logging

--- src/simulation/logger/__init__.py
+++ src/simulation/logger/__init__.py
@@ -1,5 +1,6 @@
+from src.simulation.logger.agent_logging import bind_causal_logger, bind_event_logger, causal_logger_from, event_logger_from
 from src.simulation.logger.event_logger import EventLogger
 from src.simulation.logger.initialization_logger import InitializationLogger
 from src.simulation.logger.snapshot_logger import SnapshotLogger
 
-__all__ = ["EventLogger", "InitializationLogger", "SnapshotLogger"]
+__all__ = ["EventLogger", "InitializationLogger", "SnapshotLogger", "bind_causal_logger", "bind_event_logger", "causal_logger_from", "event_logger_from"]
